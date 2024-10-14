58e06ef57e98a5c37b87e9f1121232a1e7019b6f
gautamKumar <gautamkannoujiya58@gmail.com>
2024-10-14 15:19:43 +0530

    implemented counter application as class based component

--- xclasscomp/src/App.jsx
+++ xclasscomp/src/App.jsx
@@ -1,7 +1,10 @@
+import CounterApp from "./CounterApp";
+
 function App() {
   return (
     <>
       <h1>X-ClassComp</h1>
+      <CounterApp />
     </>
   );
 }
---
+++ xclasscomp/src/CounterApp.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+    // initialization of state
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+export default CounterApp;
